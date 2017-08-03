cadb321becfbb8604baf661a1583fa4d55eb16df
乙淅君 <30467956+LazyCatTF@users.noreply.github.com>
2017-08-03 15:01:56 +0800

    Exercise 1~21

---
+++ ex13.py
@@ -0,0 +1,27 @@
+from sys import argv
+
+# script, first, second, third = argv
+
+# print "The script is called:", script
+# print "Your first variable is:", first
+# print "Your second variable is:", second
+# print "Your third variable is:", third
+
+
+# ee, eee, eeee = argv
+
+# print "MEOW", ee
+# print "WOW", eee
+# print "EU", eeee
+
+
+scriptname, KING, QWEEN, JACK, ME, FASHION = argv
+
+FASHION = raw_input("Who is my sweetheart?")
+
+print scriptname
+print ".", KING
+print QWEEN
+print JACK
+print ME
+print FASHION
