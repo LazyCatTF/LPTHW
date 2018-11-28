33991b892d0dacca7626327916f4423f1b990feb
lctfwyt <lctfwyt@outlook.com>
2018-11-28 23:02:39 +0800

    rename ex1-9 and update ex1-3 to the python3 version

---
+++ ex3.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+
+# 字符串
+print ("I will now count my chickens:")
+
+# 母鸡 公鸡 分别多少只
+print ("Hens", 25.0 + 30.0 / 6.0)
+print ("Roosters", 100.0 - 25.0 * 3.0 % 4.0)
+
+# 字符串
+print ("Now I will count the eggs:")
+
+# 鸡蛋的数量
+print (3.0 + 2.0 + 1.0 - 5.0 + 4.0 % 2.0 - 1.0 / 4.0 + 6.0)
+
+# 字符串
+print ("Is it true that 3.0 + 2.0 < 5.0 - 7.0?")
+
+# 判断不等式正误
+print (3.0 + 2.0 < 5.0 - 7.0)
+
+# 分别计算不等式两边数值
+print ("What is 3.0 + 2.0?", 3.0 + 2.0)
+print ("What is 5.0 - 7.0?", 5.0 - 7.0)
+
+# 字符串
+print ("Oh, that's why it's False.")
+
+# 字符串
+print ("How about some more.")
+
+# 字符串 以及不等式正误
+print ("Is it greater?", 5.0 > -2.0)
+print ("Is it greater or equal?", 5.0 >= -2.0)
+print ("Is it less or equal?", 5.0 <= -2.0)
