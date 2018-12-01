b61ce5e015c7ca0a19e380813eb8e6b8d7bc5d8f
lctfwyt <lctfwyt@outlook.com>
2018-12-01 23:45:06 +0800

    Update ex15-20 to python3

--- ex15.py
+++ ex15.py
@@ -1,15 +1,17 @@
-# from sys import argv
+from sys import argv
 
-# script, filename = argv
+script, filename = argv
 
-# txt = open(filename)
+txt = open(filename)
 
-# print "Here's your file %r:" % filename
-# print txt.read()
+print (f"Here's your file {filename}:")
+print (txt.read())
+txt.close()
 
-print "Type the filename again:"
-file_again = raw_input("> ")
+print ("Type the filename again:")
+file_again = input("> ")
 
 txt_again = open(file_again)
 
-print txt_again.read()
+print (txt_again.read())
+txt_again.close()
--- ex16.py
+++ ex16.py
@@ -1,31 +1,32 @@
 from sys import argv
 
 script, filename = argv
-# print "We're going to erase %r." % filename
-# print "If you don't want that, hit CTRL+C (^C)."
-# print "If you do want that, hit RETURN."
 
-# raw_input("?")
+# print(f"We're going to erase {filename}.")
+# print("If you don't want that, hit CTRL+C (^C).")
+# print("If you do want that, hit RETURN.")
 
-print "Opening the file..."
-target = open(filename, 'w')
+# input("?")
 
-# print "Truncating the file. Goodbye!"
+print("Opening the file...")
+target = open(filename, 'w+')
+
+# print("Truncating the file. Goodbye!")
 # target.truncate()
 
-print "Now I'm going to ask you for three lines."
+print("Now I'm going to ask you for three lines.")
 
-line1 = raw_input("line1:")
-line2 = raw_input("line2:")
-line3 = raw_input("line3:")
+line1 = input("line 1: ")
+line2 = input("line 2: ")
+line3 = input("line 3: ")
 
-print "I'm going to write these to the file."
+print("I'm going to write these to the file.")
 
-target.write(line1 + "\n" + line2 + "\n" + line3 + "\n")
+target.write(f"{line1}\n{line2}\n{line3}\n")
+target.seek(0)
 
-print "Now let's see what's in the file now."
-target = open(filename)
-print target.read()
+print("Now let's see what's in the file now.")
+print(target.read())
 
-print "And finally, we close it."
+print("And finally, we close it.")
 target.close()
